22ab4e77f41d3a7b9bc0b58addb9c1830fc904a7
JesueJunior <jesuesousa@gmail.com>
2014-10-18 18:48:10 -0300

    Removendo barra no fim das urls, ainda em testes

--- cloudops/urls.py
+++ cloudops/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from api.views import ServerList, ServerNew, ApplicationNew, ApplicationList, ApplicationEdit, ApplicationDelete, ApplicationDetail, ServerDetail, ServerEdit, ServerDelete
 
@@ -11,25 +13,24 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^authentication/', 'rest_framework.authtoken.views.obtain_auth_token', name='auth'),
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 )
 
 
 urlpatterns += patterns('api.views.server',
-                        url(r'^servers/$', ServerList.as_view(), name='server-list'),
-                        url(r'^servers/new/$', ServerNew.as_view(), name='server-new'),
+                        url(r'^servers$', ServerList.as_view(), name='server-list'),
+                        url(r'^servers/new$', ServerNew.as_view(), name='server-new'),
                         url(r'^servers/(?P<pk>[\d]+)/detail$', ServerDetail.as_view(), name='server-detail'),
                         url(r'^servers/(?P<pk>[\d]+)/edit$', ServerEdit.as_view(), name='server-edit'),
                         url(r'^servers/(?P<pk>[\d]+)/delete$', ServerDelete.as_view(), name='server-delete'),
-
 )
 
 urlpatterns += patterns('api.views.application',
-                        url(r'^applications/$', ApplicationList.as_view(), name='application-list'),
-                        url(r'^applications/new/$', ApplicationNew.as_view(), name='application-new'),
+                        url(r'^applications$', ApplicationList.as_view(), name='application-list'),
+                        url(r'^applications/new$', ApplicationNew.as_view(), name='application-new'),
                         url(r'^applications/(?P<pk>[\d]+)/detail$', ApplicationDetail.as_view(), name='application-detail'),
                         url(r'^applications/(?P<pk>[\d]+)/edit$', ApplicationEdit.as_view(), name='application-edit'),
                         url(r'^applications/(?P<pk>[\d]+)/delete$', ApplicationDelete.as_view(), name='application-delete'),
-
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
